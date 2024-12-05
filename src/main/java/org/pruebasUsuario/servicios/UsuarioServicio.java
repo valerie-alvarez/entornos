9cdf356d50ecc93379939891dda906b1a260036c
NataliaOsorioL <126190992+NataliaOsorioL@users.noreply.github.com>
2024-12-05 18:18:06 -0500

    usuario serviciio documen

--- src/main/java/org/pruebasUsuario/servicios/UsuarioServicio.java
+++ src/main/java/org/pruebasUsuario/servicios/UsuarioServicio.java
@@ -5,6 +5,9 @@ import org.pruebasUsuario.modelo.Usuario;
 import org.pruebasUsuario.modelo.repositorio.UsuarioRepositorio;
 import java.time.LocalDate;
 
+ /*Constructor que inicializa el servicio de usuario. 
+y UsuarioRepositorio es el repositorio para gestionar los usuarios. */
+
 public class UsuarioServicio {
 
         private UsuarioRepositorio usuarioRepositorio;
@@ -13,24 +16,29 @@ public class UsuarioServicio {
             this.usuarioRepositorio = usuarioRepositorio;
         }
 
+//Registrar cliente nuevo en el sistema.
     public Usuario registrarCliente(Cliente cliente){
             validarCorreoUnico(cliente.getCorreo());
             usuarioRepositorio.guardar(cliente);
             return usuarioRepositorio.buscarporCorreo(cliente.getCorreo());
     }
-
+        
+//Registrar cliente nuevo en el sistema.
     public Admin registrarAdmin(Admin admin) {
         validarCorreoUnico(admin.getCorreo());
         usuarioRepositorio.guardar(admin);
         return usuarioRepositorio.buscarporCorreo(admin.getCorreo());
     }
 
+/*Verifica que el correo ingresado no esté registrado
+previamente en el sistema. */
     private void validarCorreoUnico(String correo) {
         if (usuarioRepositorio.buscarporCorreo(correo) != null) {
             throw new IllegalArgumentException("El correo ya está registrado");
         }
     }
 
+//Inicio de sesión de un usuario en el sistema.
     public Usuario iniciarSesion(String correo, String contrasena) {
             Usuario usuario = usuarioRepositorio.buscarporCorreoyContrasena(correo, contrasena);
             if (usuario == null) {
@@ -38,7 +46,8 @@ public class UsuarioServicio {
             }
             return usuario;
         }
-
+        
+//Actualizacion de perfil de un usuario
     public Cliente actualizarPerfil(String correo, String nuevoNombre, String nuevoPasaporte) {
         Cliente cliente = usuarioRepositorio.buscarporCorreo(correo);
         if (cliente == null) {
